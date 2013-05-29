e08896df4a57107f3eb83e9b81a5f35df5ee9d64
jennybc <jenny@stat.ubc.ca>
2013-05-29 12:47:54 -0700

    Implementing Yihui Xie's advice for using knitr::stitch_rhtml from command line or in a Makefile (see issue #2).
    Basic advice: initiate knitr::stitich_rhtml in the directory where you want output.
    Caveat: figure still not appearing when I view the html in browser, but hopefully I'm getting closer?

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: results/avgX.txt figs/slopes_AsiaVsAmericas.pdf prose/02_slopeComparisonAsiaVsAmericas.html
+  all: results/avgX.txt figs/slopes_AsiaVsAmericas.pdf prose/02_slopeComparisonAsiaVsAmericas.html
 
 results/avgX.txt figs/niftyPlot.pdf: data/gapminderDataFiveYear.txt code/block01_toyLine.R
 	Rscript code/block01_toyLine.R
@@ -7,10 +7,11 @@ results/gCoef.txt results/gCoef.rds: data/gapminderDataFiveYear.txt code/01_coun
 	Rscript code/01_countrySpecificInterceptSlope.R
 
 prose/02_slopeComparisonAsiaVsAmericas.html: results/gCoef.rds code/02_slopeComparisonAsiaVsAmericas.R
-	Rscript -e "knitr::stitch_rhtml('code/02_slopeComparisonAsiaVsAmericas.R', output = 'prose/02_slopeComparisonAsiaVsAmericas.html')"
+	cd prose;        \
+  Rscript -e "knitr::stitch_rhtml('../code/02_slopeComparisonAsiaVsAmericas.R', output = '02_slopeComparisonAsiaVsAmericas.html')"
 
 figs/slopes_AsiaVsAmericas.pdf results/slopes_AsiaVsAmericas.txt: results/gCoef.rds code/03_slopeComparisonAsiaVsAmericas.R
 	Rscript code/03_slopeComparisonAsiaVsAmericas.R
 
 clean:
-	rm -f results/* figs/*
+	rm -f results/* figs/* prose/02_slopeComparisonAsiaVsAmericas.html
--- code/02_slopeComparisonAsiaVsAmericas.R
+++ code/02_slopeComparisonAsiaVsAmericas.R
@@ -2,24 +2,28 @@
 ## or click notebook button in top right of editor pane
 
 ## or get same result from the command line:
-## Rscript -e "knitr::stitch_rhtml('code/02_slopeComparisonAsiaVsAmericas.R', output = 'prose/02_slopeComparisonAsiaVsAmericas.html')"
+
+## assuming that this file lives in code/ and you want the result in prose/
+## and that working directory is prose/
+
+## Rscript -e "knitr::stitch_rhtml('../code/02_slopeComparisonAsiaVsAmericas.R', output = '02_slopeComparisonAsiaVsAmericas.html')"
 
 ## either will require knitr package to work
 ## to install: install.packages("knitr")
 
 library(lattice)
 
-## the working directory will differ for live execution vs. command line execution vs.
-## using Compile Notebook
-## you will need to toggle the comment status of these lines to get
+## ALERT: the working directory may differ for live execution vs. command line execution vs.
+## using RStudio's Compile Notebook.
+
+## If things aren't working, scrutinize the working directory.
+## Is it what you think it is?
+getwd()
+
+## you may need to toggle the comment status of these lines to get
 ## what you want
-
-## for Compile Notebook:
-## str(gCoef <- readRDS("../results/gCoef.rds"))
-
-## for live execution or command line execution, when working directory set
-## correctly to top-level directory for this project
-str(gCoef <- readRDS("results/gCoef.rds"))
+## str(gCoef <- readRDS("results/gCoef.rds"))
+str(gCoef <- readRDS("../results/gCoef.rds"))
 
 hDat <-
   droplevels(subset(gCoef,
