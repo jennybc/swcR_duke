6e7f4d65ea13dcef971f779ce912666ab3d0addb
jennybc <jenny@stat.ubc.ca>
2013-08-22 22:54:41 -0700

    Light editing of Block 2. Early draft of polished presentation of Block 2 (-ish) material.

--- code/block02_careFeedingData.R
+++ code/block02_careFeedingData.R
@@ -72,6 +72,10 @@ xyplot(lifeExp ~ gdpPercap, gDat,
        subset = year == 2007, auto.key = TRUE)
 
 ## back to vetting a recently imported data.frame ...
+## data.frame = special case of list
+## length of each component (= variable) must be same
+## different components (= variables) can be of different flavor
+## pull out one variable with $
 str(gDat)
 summary(gDat)
 gDat$lifeExp # dollar sign is how you access 1 variable
@@ -80,16 +84,25 @@ densityplot(~ lifeExp, gDat)
 
 ## factors: the R objects you love to hate
 ## JB's advice: use them! learn how to handle safely
+class(gDat$continent)
 table(gDat$continent)
 summary(gDat$continent)
 levels(gDat$continent)
 str(gDat)
+
 ## note that the actual *values* are integers, not the character codes,
 ## e.g. Africa or Europe, that are more user-visible
 ## never ever ever forget that factors are SPECIAL
+
+## factors are two-faced schizo booby traps waiting for you!
+## levels vs. integer codes
+## worth the hassle, think about our use of y ~ x | z and y ~ x, ..., groups = z
+## will have big payoffs in modelling too
+
 nlevels(gDat$continent)
 barchart(table(gDat$continent))
 dotplot(table(gDat$continent), type = "h")
+dotplot(table(gDat$continent), type = c("p", "h"))
 
 ## if you want just some rows and/or just some variables, for inspection or to
 ## assign as a new object, use subset()
@@ -101,6 +114,10 @@ subset(gDat, subset = country == "Uruguay",
 subset(gDat, subset = country != "United States",
        select = c(country, lifeExp))
 
+## ponder the subset() doc, subset and select args
+## their turn:
+## get data just for 2007
+## how many rows? how many observations per continent? remake plot(s)
 ## notice the similarity of interface for subset(), xyplot(), lm(), ...
 ## this is not a coincidence!
 
@@ -121,6 +138,9 @@ summary(myFit)
 myFit <- lm(lifeExp ~ I(year - minYear), gDat,
             subset = country == "Cambodia")
 summary(myFit)
+
+xyplot(lifeExp ~ I(year - minYear), gDat,
+       subset = country == "Cambodia", type = c("p", "r"))
 
 ## if a function does not have a "data" argument, with() can be handy
 ## if you are addicted to attach(), which is to be avoided!, with() is your
@@ -192,3 +212,22 @@ which(x < 0)
 x[c(TRUE, FALSE)]
 x["b"]
 x[c("b","f")]
+
+## possible things to add in future
+## show use of I() to protect character info from conversion to factor
+## show indexing a list with double square brackets
+## compare indexing a data.frame w/ single bracket vs
+## double vs matrix style
+
+## accessing one variable
+#jDat$z
+## matrix-style
+#jDat[ , "z"]
+## vector style
+#jDat["z"]
+#jDat[c("w", "z")]
+## list style
+#jDat[["z"]]
+#class(jDat[["z"]])
+#class(jDat["z"])
+
